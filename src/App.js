19a318b66cfa0fa0b484dc075e15f41f9164e62c
samueltumewu <samueltumewu21@gmail.com>
2024-04-20 20:29:20 +0700

    ref(*): restructure react stateless components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import { RunningShoesTables } from './components/RunningShoesTable';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <RunningShoesTables />
+  const [shoesData, setShoesData] = useState([]);
+  const [shoesDataHeaders, setShoesDataHeaders] = useState([]);
+  const [shoesPageResponseStatus, setShoesPageResponseStatus] = useState(-1);
+  const [apiResponseError, setApiResponseError] = useState('');
+
+  const resetAllStates = () => {
+    setShoesData([]);
+    setShoesDataHeaders([]);
+    setShoesPageResponseStatus(-1);
+    setApiResponseError('');
+  }
+
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('https://store-catalog-app-1-0-0-snapshot.onrender.com/shoes?page=2');
+          console.log('response entries: ' )
+          console.log(Object.entries(response))
+          const API_RESPONSE_DATA = response.data;
+          setShoesPageResponseStatus(response.status)
+          if (API_RESPONSE_DATA.success) {
+            setShoesData(API_RESPONSE_DATA.data);
+            setShoesDataHeaders(Object.keys(API_RESPONSE_DATA.data[0]));
+          } 
+        } catch (error) {
+          console.error('Error fetching data:', error.message);
+          setApiResponseError(error.message);
+        }
+      };
+      fetchData();
+      return resetAllStates()
+    },[])
+
+  let appPage = (
+      <div className="App">
+            <div>
+              <RunningShoesTables 
+                shoesData={shoesData} 
+                shoesDataHeaders={shoesDataHeaders} 
+                shoesPageResponseStatus={shoesPageResponseStatus}/>
+            </div>
       </div>
-    </div>
-  );
+  )
+
+  if(shoesPageResponseStatus!=-1) {
+    return appPage;
+  } else {
+    return (
+      <>
+        <p>{apiResponseError}</p>
+        {!apiResponseError && 
+          <div className='center-p-in-div'>
+            <p>Wait a moment...<br />Our backend service on a free container is still starting haha😅</p>
+            <p>Maybe take 1-3 minutes...</p>
+          </div>}
+      </>
+    )
+  }
 }
 
 export default App;
--- src/components/RunningShoesTable.js
+++ src/components/RunningShoesTable.js
@@ -1,61 +1,50 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
 import './RunningShoesTable.css';
 
-export const RunningShoesTables = () => {
-    const [shoesData, setShoesData] = useState([]);
-    const [responseHeaders, setResponseHeaders] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get('https://store-catalog-app-1-0-0-snapshot.onrender.com/shoes?page=2');
-            console.log(Object.keys(response.data))
-            const API_RESPONSE_DATA = response.data;
-            if (API_RESPONSE_DATA.success) {
-              setShoesData(API_RESPONSE_DATA.data);
-              setResponseHeaders(Object.keys(API_RESPONSE_DATA.data[0]));
-            } 
-            // console.log(`shoesData: ${API_RESPONSE_DATA}`)
-            // console.log(`shoesData: ${Object.keys(shoesData[0])}`)
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-
-        fetchData();
-
-
-      },[])
-
-    return (
+export const RunningShoesTables = ({shoesData, shoesDataHeaders, shoesPageResponseStatus}) => 
+{    
+    let defaultPage = 
+      (
         <>
-            <h1>hello</h1>
-            <div className="center-div">
-            <table>
-              <thead>
-                <tr>
-                    {responseHeaders.map(header => <th key={header}>{header}</th>)}
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  shoesData.map((currData, index) => (
-                      <tr key={index}>
-                        {
-                          Object.values(currData).map(
-                            (cell, index) => <td key={index}>{cell}</td>
-                          )
-                        }
-                      </tr>
-                      ))
-                }
-              </tbody>
-            </table>
-            </div>         
-            
-            {/* <p>{shoesData}</p> */}
-            {/* {shoesData.map((value)=><p>{value.size}</p>)} */}
+          <h1>hello</h1>
+          <div className="center-div">
+          <table>
+            <thead>
+              <tr>
+                  {shoesDataHeaders.map(header => <th key={header}>{header}</th>)}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                shoesData.map((currData, index) => 
+                  (<tr key={index}>
+                      {
+                        Object.values(currData).map((cell, index) => 
+                          <td key={index}>{cell}</td>
+                        )
+                      }
+                    </tr>
+                  ))
+              }
+            </tbody>
+          </table>
+          </div>         
         </>
-    );
+      )
+
+    let notFoundTable = (
+      <>
+        <h1>No table found</h1>
+      </>
+    )
+    
+    switch (shoesPageResponseStatus) {
+      case 200:
+        return shoesData.length > 1 ? defaultPage : notFoundTable
+      case 400:
+        return notFoundTable
+      case -1:
+        return <p>Loading...</p>
+      default:
+        return <p>WHOAAA</p>
+    }
 }
