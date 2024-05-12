867cc28fc2d7ce2ede889a9ef113da5c26bd4a95
samueltumewu <samueltumewu21@gmail.com>
2024-05-12 16:46:58 +0700

    feat(*): quantity action buttons, fix api requests

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { RunningShoesTables } from './components/RunningShoesTable';
 
 function App() {
+  const BASE_URL = 'https://store-catalog-app-1-0-0-snapshot.onrender.com/';
   const [shoesData, setShoesData] = useState([]);
   const [shoesDataHeaders, setShoesDataHeaders] = useState([]);
   const [shoesPageResponseStatus, setShoesPageResponseStatus] = useState(-1);
@@ -14,6 +15,51 @@ function App() {
   const [perPage, setPerPage] = useState(10);
   const [countPagination, setCountPagination] = useState(0);
 
+  // HTTP Handling
+  const fetchData = async () => {
+    try {
+      let urlGet = `${BASE_URL}/shoes?page=${pageNumber}&per_page=${perPage}`
+      const response = await axios.get(urlGet);
+      const API_RESPONSE_DATA = response.data;
+      setShoesPageResponseStatus(response.status)
+      if (API_RESPONSE_DATA.success) {
+        setShoesData(API_RESPONSE_DATA.data);
+        setShoesDataHeaders(Object.keys(API_RESPONSE_DATA.data[0]));
+        setCountPagination(API_RESPONSE_DATA.count);
+      } 
+      console.log(JSON.stringify(shoesData, null, 4))
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      setApiResponseError(error.message);
+    }
+  };
+
+  const putQuantity = async (content, quantity) => {
+    let isSuccess = false;
+    const rollbackQuantityValue = content.quantity;
+    const shoesId = content.id;
+    try {
+      let urlPutQuantity = `${BASE_URL}/shoes/id/${shoesId}/quantity/${quantity}`
+      // const configHeaders = { headers: {'Content-Type': 'application/json'} };
+      const response = await axios.put(urlPutQuantity);
+      isSuccess = response.data.success;
+      // console.log(JSON.stringify(response, null, 4));
+    } catch (error) {
+      console.error('Error http put:', error);
+      isSuccess = error.response.response.data.success;
+      setApiResponseError(error.message);
+    } finally {
+      console.log('<<putQuantity Result: ' + isSuccess);
+      if (!isSuccess) {
+        setShoesData(shoes => shoes.map(item =>
+          item.id === content.id ? { ...item, 'quantity': rollbackQuantityValue } : item
+        ))
+        console.debug(`<<rollback done`)
+      }
+      return isSuccess;
+    }
+  };
+
   const resetAllStates = () => {
     setShoesData([]);
     setShoesDataHeaders([]);
@@ -21,23 +67,38 @@ function App() {
     setApiResponseError('');
   }
 
-  useEffect(() => {
-      const fetchData = async () => {
-        try {
-          let urlGet = `https://store-catalog-app-1-0-0-snapshot.onrender.com/shoes?page=${pageNumber}&per_page=${perPage}`
-          const response = await axios.get(urlGet);
-          const API_RESPONSE_DATA = response.data;
-          setShoesPageResponseStatus(response.status)
-          if (API_RESPONSE_DATA.success) {
-            setShoesData(API_RESPONSE_DATA.data);
-            setShoesDataHeaders(Object.keys(API_RESPONSE_DATA.data[0]));
-            setCountPagination(API_RESPONSE_DATA.count);
-          } 
-        } catch (error) {
-          console.error('Error fetching data:', error.message);
-          setApiResponseError(error.message);
+  const modifyQuantity = (action, rowData) => {
+    let currentQuantity = Number(rowData.quantity);
+    let currentId = Number(rowData.id);
+    switch(action) {
+      case 'increment':
+        currentQuantity++;
+        setShoesData(shoes => {
+          return shoes.map(item =>
+            item.id === currentId ? { ...item, 'quantity': currentQuantity } : item
+          )
+        })
+        putQuantity(rowData, currentQuantity)
+        break;
+      case 'decrement':
+        if (currentQuantity > 0) {
+          currentQuantity--;
+          setShoesData(shoes => {
+            return shoes.map(item =>
+              item.id === currentId ? { ...item, 'quantity': currentQuantity } : item
+            )
+          })
+          putQuantity(rowData, currentQuantity)
+        } else {
+          console.log("Decrement action invalid to be invoked!")
         }
-      };
+        break;
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
       fetchData();
       return resetAllStates()
     },[pageNumber, perPage])
@@ -53,7 +114,8 @@ function App() {
                 setPageNumber={setPageNumber}
                 perPage={perPage}
                 setPerPage={setPerPage}
-                countPagination={countPagination}/>
+                countPagination={countPagination}
+                modifyQuantity={modifyQuantity}/>
             </div>
       </div>
   )
